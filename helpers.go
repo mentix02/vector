bbaf017ebcf882753b269e9c8ca5a36458b169a2
mentix02 <manan.yadav02@gmail.com>
2020-03-29 23:41:34 -0700

    Added method for checking equality between two vectors.

--- helpers.go
+++ helpers.go
@@ -1,6 +1,6 @@
 package vector
 
-// EqualsSlice checks for equality between two
+// EqualsSlice checks for equality between two slices.
 func (v *Vector) EqualsSlice(arr []interface{}) bool {
 
 	if v.Len() != uint64(len(arr)) {
@@ -9,6 +9,22 @@ func (v *Vector) EqualsSlice(arr []interface{}) bool {
 
 	for index, item := range v.Range() {
 		if arr[index] != item {
+			return false
+		}
+	}
+
+	return true
+}
+
+// EqualsVector checks for equality between two vectors.
+func (v *Vector) EqualsVector(ov Vector) bool {
+
+	if v.Len() != ov.Len() {
+		return false
+	}
+
+	for index, item := range v.Range() {
+		if ov.At(uint64(index)) != item {
 			return false
 		}
 	}
